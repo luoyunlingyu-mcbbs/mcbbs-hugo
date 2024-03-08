e1a1e8cac02976c0970487d35188b5f6fca9d86b
dimo <forker4@dim0.uu.me>
2024-03-08 22:22:54 +0800

    1st update

---
+++ ren.sh
@@ -0,0 +1,8 @@
+cd "$1"
+for file in *.html; do
+    filename=$(basename "$file")
+    # 读取文件名并将其添加到文件的第一行
+    sed -i "1i ---" "$file"
+    sed -i "1s|^|title: \"$filename\"\n|" "$file"
+    sed -i "1i ---" "$file"
+done
