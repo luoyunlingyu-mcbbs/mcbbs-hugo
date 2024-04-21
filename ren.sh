658a35a7cd42c2e1c546d93aa9a0e35f295bcb79
dimo <forker4@dim0.uu.me>
2024-04-21 15:07:55 +0800

    添加时间

--- ren.sh
+++ ren.sh
@@ -1,8 +1,10 @@
 cd "$1"
+export timenow=$(TZ=UTC-8 date -Iseconds)
 for file in *.html; do
     filename=$(basename "$file")
     # 读取文件名并将其添加到文件的第一行
     sed -i "1i ---" "$file"
+    sed -i "1s|^|date: $timenow \n|" "$file"
     sed -i "1s|^|title: \"$filename\"\n|" "$file"
     sed -i "1i ---" "$file"
 done
@@ -10,6 +12,7 @@ for file in *.htm; do
     filename=$(basename "$file")
     # 读取文件名并将其添加到文件的第一行
     sed -i "1i ---" "$file"
+    sed -i "1s|^|date: $timenow \n|" "$file"
     sed -i "1s|^|title: \"$filename\"\n|" "$file"
     sed -i "1i ---" "$file"
 done
